a5498bf5b924f503ae706982560f8c63f6acf7db
Olivia Culver <oculv21@gmail.com>
2019-04-09 19:10:18 -0400

    background color changes with quote and load quote button color changed

--- js/script.js
+++ js/script.js
@@ -4,40 +4,49 @@ let quotes = [
     quote: "Bringing the gifts that my ancestors gave, I am the dream and the hope of the slave. I rise. I rise. I rise.",
     source: "Maya Angelou",
     citation: "Still I Rise",
-    year: 1978
+    year: 1978,
+    color: "#BF3FBF"
   },
   {
     quote: "Success is to be measured not so much by the position that one has reached in life as by the obstacles which he has overcome while trying to succeed.",
-    source: "Booker T. Washington"
+    source: "Booker T. Washington",
+    color: "#BF3F3F"
   },
   {
-    quote: "We must develop and maintain the capacity to forgive. He who is devoid of the power to forgive is devoid of the power to love. There is some good in the worst of us and some evil in the best of us. When we discover this, we are less prone to hate our enemies.",
-    source: "Dr. Martin Luther King Jr."
+    quote: "If you can't fly then run, if you can't run then walk, if you can't walk then crawl, but whatever you do you have to keep moving forward.",
+    source: "Dr. Martin Luther King Jr.",
+    color: "#7FBF3F"
   },
   {
     quote: "Change will not come if we wait for some other person or some other time. We are the ones we’ve been waiting for. We are the change that we seek.",
     source: "Barack Obama",
-    year: 2008
+    year: 2008,
+    color: "#3F7FBF"
   },
   {
-    quote: "One of the lessons that I grew up with was to always stay true to yourself and never let what somebody else says distract you from your goals. And so when I hear about negative and false attacks, I really don’t invest any energy in them, because I know who I am.",
-    source: "Michelle Obama"
+    quote: "When they go low, we go high.",
+    source: "Michelle Obama",
+    color: "#BF3F7F"
   },
   {
     quote: "When I dare to be powerful – to use my strength in the service of my vision, then it becomes less and less important whether I am afraid.",
-    source: "Audre Lorde"
+    source: "Audre Lorde",
+    color: "#BFBF3F"
   },
   {
     quote: "Never be limited by other people’s limited imaginations.",
-    source: "Dr. Mae Jemison"
+    source: "Dr. Mae Jemison",
+    color: "#3FBFBF"
   },
   {
     quote: "If there is no struggle, there is no progress.",
-    source: "Frederick Douglass"
+    source: "Frederick Douglass",
+    color: "#BF7F3F"
   },
   {
     quote: "You’re not to be so blind with patriotism that you can’t face reality. Wrong is wrong, no matter who does it or says it.",
-    source: "Malcolm X"
+    source: "Malcolm X",
+    color: "#3F3FBF"
   }
 ]
 
@@ -48,8 +57,6 @@ function getRandomQuote() {
 }
 
 //print random quote 
-let intervalID = window.setInterval(printQuote, 8000);
-
 function printQuote() {
   let newQuote = getRandomQuote();
   let quoteBox = '';
@@ -63,7 +70,8 @@ function printQuote() {
   }
   quoteBox += '</p>';
   document.getElementById("quote-box").innerHTML = quoteBox;
+  document.body.style.backgroundColor = newQuote.color;
 }
-
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+let intervalID = window.setInterval(printQuote, 8000);
 
